ba104b9a3acaa926351fd4dc42b0bbf177503771
rendatinkpu <lrendatin@gmail.com>
2026-05-11 11:04:27 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Ganti URL ini jika URL Apps Script Web App berubah.
-window.APP_CONFIG = {
-  appName: "Bukti Dukung",
-  appShortName: "Bukti",
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxKbvrkWow4UFEHWj_DrqpF5r3-2A8WMKqa5i9s3zymxSOijilpycJ_F6iL1VGeH-qu4g/exec"
-};
