78f0d15e6adbcd58c944ea1fa6def4cdc56554b4
rendatinkpu <lrendatin@gmail.com>
2026-05-12 11:44:52 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,53 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'digital-synergy-pwa-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './service-worker.js'
+];
+
+self.addEventListener('install', event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const responseCopy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseCopy));
+        return response;
+      });
+    })
+  );
 });
